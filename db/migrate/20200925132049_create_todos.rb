814bd78d7279cebc90c649f0ea46c77c679ab88e
Timofeev Yurii <georgius@ural.ru>
2020-09-25 18:32:17 +0500

    added todos model

--- db/migrate/20200925132049_create_todos.rb
+++ db/migrate/20200925132049_create_todos.rb
@@ -1,9 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
+      t.integer :row
       t.text :task
       t.boolean :done
-
+      t.string :icon
       t.timestamps
     end
   end
