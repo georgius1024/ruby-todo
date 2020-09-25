420472bac43fb7d85ffd9e4170aa7cd4a58f5619
Timofeev Yurii <georgius@ural.ru>
2020-09-26 01:59:33 +0500

    todos controller

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,74 +1,74 @@
 class Api::TodosController < ApplicationController
-  before_action :set_api_todo, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+  # before_action :set_api_todo, only: [:show, :create, :update, :destroy]
 
   # GET /api/todos
   # GET /api/todos.json
   def index
-    @api_todos = Api::Todo.all
+    @todos = Todo.all
+    render json: @todos, status: 200
   end
 
   # GET /api/todos/1
   # GET /api/todos/1.json
   def show
-  end
-
-  # GET /api/todos/new
-  def new
-    @api_todo = Api::Todo.new
-  end
-
-  # GET /api/todos/1/edit
-  def edit
+    @todo = Todo.find(params[:id])
+    render json: @todo
   end
 
   # POST /api/todos
   # POST /api/todos.json
   def create
-    @api_todo = Api::Todo.new(api_todo_params)
+    # render json: api_todo_params
+    # render json: params
+    @todo = Todo.new(api_todo_params)
 
-    respond_to do |format|
-      if @api_todo.save
-        format.html { redirect_to @api_todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @api_todo }
-      else
-        format.html { render :new }
-        format.json { render json: @api_todo.errors, status: :unprocessable_entity }
-      end
+    @todo.assign_attributes(api_todo_params)
+    if @todo.save
+      render json: @todo, status: :created
+    else
+      render json: @todo.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/todos/1
   # PATCH/PUT /api/todos/1.json
   def update
-    respond_to do |format|
-      if @api_todo.update(api_todo_params)
-        format.html { redirect_to @api_todo, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @api_todo }
-      else
-        format.html { render :edit }
-        format.json { render json: @api_todo.errors, status: :unprocessable_entity }
-      end
+    @todo = Todo.find(params[:id])
+    if @todo.update(api_todo_params)
+      render json: @todo, status: :ok
+    else
+      render json: @todo.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/todos/1
   # DELETE /api/todos/1.json
   def destroy
-    @api_todo.destroy
-    respond_to do |format|
-      format.html { redirect_to api_todos_url, notice: 'Todo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @todo = Todo.find(params[:id])
+  rescue StandardError => e
+    send_error(e, 404)
+  else
+    @todo.destroy
+    render json: {}, status: :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_todo
-      @api_todo = Api::Todo.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def api_todo_params
-      params.fetch(:api_todo, {})
+  # Use callbacks to share common setup or constraints between actions.
+  def set_api_todo
+    if params[:id]
+      @api_todo = Todo.find(params[:id])
     end
+  end
+
+  # Only allow a list of trusted parameters through.
+  def api_todo_params
+    params.require(:todo).permit(:row, :task, :done, :icon)
+    # $params.slice(:row, :task, :done, :icon)
+  end
+
+  def send_error(message, status)
+    render json: { message: message }, status: status
+  end
 end
