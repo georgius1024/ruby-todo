58ead109fa862327cac03af617f0828cbf991e75
Timofeev Yurii <georgius@ural.ru>
2020-09-28 10:02:29 +0500

    better error reporting

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -3,26 +3,23 @@ class Api::TodosController < ApplicationController
   # before_action :set_api_todo, only: [:show, :create, :update, :destroy]
 
   # GET /api/todos
-  # GET /api/todos.json
   def index
     @todos = Todo.all
-    render json: @todos, status: 200
+    render json: @todos, status: :ok
   end
 
   # GET /api/todos/1
-  # GET /api/todos/1.json
   def show
     @todo = Todo.find(params[:id])
+  rescue StandardError => e
+    send_error(e, :not_found)
+  else
     render json: @todo
   end
 
   # POST /api/todos
-  # POST /api/todos.json
   def create
-    # render json: api_todo_params
-    # render json: params
     @todo = Todo.new(api_todo_params)
-
     @todo.assign_attributes(api_todo_params)
     if @todo.save
       render json: @todo, status: :created
@@ -32,9 +29,11 @@ class Api::TodosController < ApplicationController
   end
 
   # PATCH/PUT /api/todos/1
-  # PATCH/PUT /api/todos/1.json
   def update
     @todo = Todo.find(params[:id])
+  rescue StandardError => e
+    send_error(e, :not_found)
+  else
     if @todo.update(api_todo_params)
       render json: @todo, status: :ok
     else
@@ -43,11 +42,10 @@ class Api::TodosController < ApplicationController
   end
 
   # DELETE /api/todos/1
-  # DELETE /api/todos/1.json
   def destroy
     @todo = Todo.find(params[:id])
   rescue StandardError => e
-    send_error(e, 404)
+    send_error(e, :not_found)
   else
     @todo.destroy
     render json: {}, status: :no_content
@@ -55,17 +53,9 @@ class Api::TodosController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_api_todo
-    if params[:id]
-      @api_todo = Todo.find(params[:id])
-    end
-  end
-
   # Only allow a list of trusted parameters through.
   def api_todo_params
     params.require(:todo).permit(:row, :task, :done, :icon)
-    # $params.slice(:row, :task, :done, :icon)
   end
 
   def send_error(message, status)
